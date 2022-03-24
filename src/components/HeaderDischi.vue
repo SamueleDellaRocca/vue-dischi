8e9fcefc5b639aef253b5daaf34be10d6eb3962b
SamueleDellaRocca <samueledellarocca95@gmail.com>
2022-03-24 19:12:45 +0100

    select

--- src/components/HeaderDischi.vue
+++ src/components/HeaderDischi.vue
@@ -2,7 +2,16 @@
   <div class="container-fluid sfondo">
     <div class="row align-items-center altezza">
       <div class="col-10"><h1 class="text-white">Spotify</h1></div>
-      <div class="col-2"><h1 class="text-white">Spotify</h1></div>
+      <div class="col-2">
+        <label class="text-white p-1" for="cars">Generi Musicali:</label>
+        <select class="p-1 rounded" name="genere" id="genere">
+          <option default value="general">General</option>
+          <option value="rock">Rock</option>
+          <option value="pop">Pop</option>
+          <option value="metal">Metal</option>
+          <option value="jazz">Jazz</option>
+        </select>
+      </div>
     </div>
   </div>
 </template>
